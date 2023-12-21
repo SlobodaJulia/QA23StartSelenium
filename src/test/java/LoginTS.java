800c3d90fbc4cac67be1a212047a361df7d4c620
SlobodaJulia <slobodajulia29@gmail.com>
2023-12-21 10:35:36 +0200

    LoginTest added

---
+++ src/test/java/LoginTS.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTS {
+
+   @BeforeClass
+    public void preCondition(){
+        //open browser and site
+    }
+@Test
+    public void loginSuccess(){
+        //open form
+        //fill form
+        //submit form
+
+    }
+    @Test
+    public void loginWrongEmail(){
+        //open form
+        //fill form
+        //submit form
+
+    }
+    @Test
+    public void loginWrongPassword(){
+        //open form
+        //fill form
+        //submit form
+
+    }
+
+  @AfterClass
+    public void postCondition(){
+        //close browser
+    }
+}
--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -2,13 +2,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.net.URL;
+
 public class StartSelenium {
     WebDriver wd;
 
     @Test
     public void test() {
         wd=new ChromeDriver();
-        wd.get("https://telranedu.web.app/");
+       // wd.get("https://telranedu.web.app/");
+        wd.navigate().to("https://telranedu.web.app/");
 
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+
+        wd.close();
+        wd.quit();
     }
 }
